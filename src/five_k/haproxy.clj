0c5a5f259bb9f612b60797e95da0a2fac0ea62bd
Prasanna Gautam <prasannagautam@gmail.com>
2015-09-13 16:37:28 -0700

    HA-proxy update works

--- src/five_k/haproxy.clj
+++ src/five_k/haproxy.clj
@@ -1,20 +1,43 @@
 (ns
+    five-k.haproxy
     "Update the HA-Proxy configuration"
-  five-k.haproxy
-  (:require [selmer.parser :as selmer]))
+    (:require [selmer.parser :as selmer]
+              [clojure.java.shell :refer [sh]]))
 
-(def haproxy-template-path "/haproxy.cfg.tmpl")
+(def haproxy-template-path "haproxy.cfg.tmpl")
 
-(def haproxy-config-system-path "/etc/haproxy.cfg")
+(def haproxy-config-system-path "/etc/haproxy/haproxy.cfg")
 
-(defn generate-config [template path & {:keys [content]
-                                        :or {:content ""}}]
-  )
+(def services (atom [{:name "backend_five_k"
+                              :servers [{:name "slave_0"
+                                         :address "10.10.4.10"
+                                         :port 8080}
+                                        {:name "slave_1"
+                                         :address "10.10.4.11"
+                                         :port 8080}]}]))
+
+(defn update-service! [name servers]
+  (let [without-service (filter #(not= (:name %)) @services)]
+    (reset! services (conj without-service {:name name
+                                            :servers servers}))))
+
+(defn generate-config [path]
+  (spit path
+        (selmer/render-file haproxy-template-path {:default-service (-> @services
+                                                                        first
+                                                                        :name)
+                                                   :services @services})))
 
 (defn reload []
   ;; reload haproxy
+  ;; correct way is hard.. just going to assume it runs as vagrant user
+  (sh "sudo" "service" "haproxy" "reload")
   )
 
-(defn update-service [service-name hosts-spec]
+(defn update-service []
   ;; Change the file in the system and reload ha-proxy
-  )
+  (let [path (str "/tmp/haproxy.cfg." (System/currentTimeMillis))
+        _ (generate-config path)
+        _ (sh "sudo" "cp" haproxy-config-system-path (str haproxy-config-system-path ".bak"))
+        _ (sh "sudo" "mv" path haproxy-config-system-path)]
+    (reload)))
